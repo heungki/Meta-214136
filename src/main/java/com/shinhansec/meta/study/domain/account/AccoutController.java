6b5145ec5f124cc8ab2433ee00002dc0770002d7
lkflower <lkflower@gmail.com>
2023-06-15 13:25:11 +0900

    spring docs 추가

--- src/main/java/com/shinhansec/meta/study/domain/account/AccoutController.java
+++ src/main/java/com/shinhansec/meta/study/domain/account/AccoutController.java
@@ -1,5 +1,10 @@
 package com.shinhansec.meta.study.domain.account;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +19,14 @@ public class AccoutController {
 
     @Autowired
     AccountService accountService;
-
+    @Operation(summary = "계좌조회", description = "계좌목록을 조회함.", tags = { "Account Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = Account.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/v1/list")
     public ArrayList accounts() {
         return accountService.findAll();
